e2a925c4f878a7680b22037bca5dbcda4e91d15b
Avnish-kumar-01 <avnishchauhan7088@gmail.com>
2026-07-17 12:51:20 +0530

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -8,7 +8,7 @@ const CONFIG = {
   // API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
   //   ? 'http://localhost:5000' 
   //   : 
-  https://avnish-portfolio-7c3y.onrender.com
+ API_URL: https://avnish-portfolio-7c3y.onrender.com
 };
 
 document.addEventListener('DOMContentLoaded', () => {
